dbc0d88c89c0aaa8ea055569cc11b68f9be85162
Henrich Offermann <henrich.offermann@gmail.com>
2020-08-20 15:36:29 +0200

    dam aj tento

---
+++ uvodny_projekt/skuska.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+excelik = pd.ExcelFile('C:/Users/HP/Documents/Python/menucka.xlsx')
+kategorie = excelik.sheet_names
+print('Kategorie su: ', kategorie)
+
+myfile=open('vypis.txt', 'w')
+
+for sheet in kategorie:
+    df=pd.read_excel(excelik, sheet_name=sheet, header=None)
+    
+    for riadok in df.values:
+        if riadok[0][-1]=='/':     #ak je na konci skutocne /
+            r=riadok[0].rsplit('/', 2)[0]
+        else:                      #ak na konci nie je /, teda ziadne alergeny
+            r=riadok[0]
+        myfile.write(' '.join((sheet, '-', r, '\n')))
+
+myfile.close()
